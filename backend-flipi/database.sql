12372fb5504161b28010e7901fa0c4cc8072f1bf
jujubssss <rodrigo.jujuba.63@gmail.com>
2025-05-06 23:52:45 -0300

    Update database.sql

--- backend-flipi/database.sql
+++ backend-flipi/database.sql
@@ -4,6 +4,6 @@ CREATE TABLE usuario (
     usuario_apelido VARCHAR(50),
     usuario_email VARCHAR(150) UNIQUE NOT NULL,
     usuario_senha VARCHAR(255) NOT NULL,
-    url_foto VARCHAR(500) NOT NULL,
+    url_foto VARCHAR(500) NULL,
     criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
