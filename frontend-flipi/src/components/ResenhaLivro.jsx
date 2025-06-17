13e61beff1c6504a299c2a4474ed00c41f26be8a
jouusey <josevitor110m.p@gmail.com>
2025-06-17 15:18:32 -0300

    uppie

--- frontend-flipi/src/components/ResenhaLivro.jsx
+++ frontend-flipi/src/components/ResenhaLivro.jsx
@@ -1,26 +1,32 @@
-import React, { useState } from 'react'
+import React from 'react'
+import "./ResenhaLivro.css"
 
-function ResenhaLivro({resenhaTitulo, resenhaTexto, usuarioId, resenhaCurtidas, usuarioApelido, resenhaNota}) {
-  
+function ResenhaLivro({ resenhaTitulo, resenhaTexto, usuarioId, resenhaCurtidas, usuarioApelido, resenhaNota }) {
 
   console.log('RESENHA TITULO: ', resenhaTitulo)
-
   console.log('RESENHA TEXTO: ', resenhaTexto)
-
   console.log('RESENHA USUARIO ID: ', usuarioId)
-
   console.log('RESENHA curtidas: ', resenhaCurtidas)
 
   return (
-    <div>
-      
-      <h2>{resenhaTitulo}</h2>
-      <h3>{usuarioApelido}</h3>
-      <h3>{resenhaTexto}</h3>
-      <h3>{resenhaNota}</h3>
-      
-      <h3>{resenhaCurtidas}</h3>
+    <div className='resenhaLivro-container'>
+      <div className="resenhaBody">
 
+        <div className="resenhaTop">
+          <label className="resenhaUsuario"> {usuarioApelido} /</label>
+          <label className="resenhaTitulo"> {resenhaTitulo}</label>
+        </div>
+
+        <div className="resenhaMiddle">
+          <label className="resenhaTexto">{resenhaTexto}</label>
+        </div>
+
+        <div className="resenhaBottom">
+          <label className="resenhaNota">NOTA : {resenhaNota}</label>
+          <label className="resenhaCurtidas"> 👍 {resenhaCurtidas}</label>
+        </div>
+
+      </div>
     </div>
   )
 }
